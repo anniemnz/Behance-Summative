b0fa63f44828d519b2da4ecfe279daf4ce957e14
Anne-marie edna Wynne <845869352@cit3-501m-08.auckland.natcoll.ac.nz>
2017-10-10 16:24:51 +1300

    graph title trial

--- js/script.js
+++ js/script.js
@@ -155,34 +155,34 @@ $(function() {
 	$.when(promise1,promise2,promise3,promise4).done(function(r1,r2,r3,r4){
 
 		let viewsData = [
-			{name: 'user1', value: r1[0].user.stats.views},
-			{name: 'user2', value: r2[0].user.stats.views},
-			{name: 'user3', value: r3[0].user.stats.views},
-			{name: 'user4', value: r4[0].user.stats.views}
+			{name: r1[0].user.username, value: r1[0].user.stats.views},
+			{name: r2[0].user.username, value: r2[0].user.stats.views},
+			{name: r3[0].user.username, value: r3[0].user.stats.views},
+			{name: r4[0].user.username, value: r4[0].user.stats.views}
 
 		];
 
 		let apprecData = [
-			{name: 'user1', value: r1[0].user.stats.appreciations},
-			{name: 'user2', value: r2[0].user.stats.appreciations},
-			{name: 'user3', value: r3[0].user.stats.appreciations},
-			{name: 'user4', value: r4[0].user.stats.appreciations}
+			{name: r1[0].user.username, value: r1[0].user.stats.appreciations},
+			{name: r2[0].user.username, value: r2[0].user.stats.appreciations},
+			{name: r3[0].user.username, value: r3[0].user.stats.appreciations},
+			{name: r4[0].user.username, value: r4[0].user.stats.appreciations}
 
 		];
 
 		let follData = [
-			{name: 'user1', value: r1[0].user.stats.followers},
-			{name: 'user2', value: r2[0].user.stats.followers},
-			{name: 'user3', value: r3[0].user.stats.followers},
-			{name: 'user4', value: r4[0].user.stats.followers}
+			{name: r1[0].user.username, value: r1[0].user.stats.followers},
+			{name: r2[0].user.username, value: r2[0].user.stats.followers},
+			{name: r3[0].user.username, value: r3[0].user.stats.followers},
+			{name: r4[0].user.username, value: r4[0].user.stats.followers}
 
 		];
 
 		let followingData = [
-			{name: 'user1', value: r1[0].user.stats.following},
-			{name: 'user2', value: r2[0].user.stats.following},
-			{name: 'user3', value: r3[0].user.stats.following},
-			{name: 'user4', value: r4[0].user.stats.following}
+			{name: r1[0].user.username, value: r1[0].user.stats.following},
+			{name: r2[0].user.username, value: r2[0].user.stats.following},
+			{name: r3[0].user.username, value: r3[0].user.stats.following},
+			{name: r4[0].user.username, value: r4[0].user.stats.following}
 
 		];
 
@@ -196,25 +196,74 @@ $(function() {
 		let viewsGraph = d3.select('#views')
 					   	.append('g');
 		viewsGraph.attr('transform','translate('+margin+','+margin+')')
+					
 							
 		var maxViews = d3.max(viewsData, function(d) { return +d.value;} );
 		var yViewsScale = d3.scaleLinear()
 				.domain([0,maxViews])
 				.range([height,0])	
 
-		viewsGraph.selectAll('rect')
+		let viewsRects = viewsGraph.selectAll('rect')
 			.data(viewsData)
 			.enter()
 			.append('rect')
+			.attr('class','bar')
 			.attr('width',50)
 			.attr('x',function(d,i){ return i*100})
 			.attr('y',function(d){ return yViewsScale(d.value) })
 			.attr('height',function(d){ return height - yViewsScale(d.value) })	
 			.attr('fill','#8ba3a6');
 
+		viewsRects.on('mouseover',function(e){
+			console.log(e);
+		})
+
 		var yAxisViewsGen = d3.axisLeft(yViewsScale).ticks(5);
 		viewsGraph.append('g')
 			.call(yAxisViewsGen);
+
+		//tooltip for views
+		var viewsTooltip = viewsGraph.append('g')
+				.style('opacity',0)
+				.attr('class','tooltip');
+
+
+		viewsTooltip.append('rect')
+				.attr('width',150)
+				.attr('height',50)
+				.attr('fill','rgba(0,0,0,0.8)');
+
+		var viewsTooltipText = viewsTooltip.append('text')
+								.text('bla')
+								.attr('class','gtext')
+								.attr('fill','white')
+								.attr('x',75)
+								.attr('y',25)
+								.style('alignment-baseline', 'middle')
+								.style('text-anchor', 'middle');
+
+		viewsBars = viewsGraph.selectAll('.bar');
+
+		//mouse events
+		viewsBars.on('mouseover',function(d){
+			viewsTooltip.style('opacity',1);
+			viewsTooltipText.text(d.name + ' : '+ d.value);
+		});
+
+		viewsBars.on('mouseout',function(d){
+			viewsTooltip.style('opacity',0);
+		});
+
+		viewsBars.on('mousemove',function(d){
+			//move the tooltip around
+			var mousePos = d3.mouse(this.parentNode);
+			var xPos = mousePos[0]-75;
+			var yPos = mousePos[1]-60;
+
+			viewsTooltip.attr('transform','translate('+xPos+','+yPos+')');
+
+
+		});
 
 
 // ===========  GRAPH-2 APPRECIATIONS  ============
@@ -224,25 +273,72 @@ $(function() {
 	apprecGraph.attr('transform','translate('+margin+','+margin+')')
 
 	var maxApprec = d3.max(apprecData, function(d) { return +d.value;} );
-		var yApprecScale = d3.scaleLinear()
-				.domain([0,maxApprec])
-				.range([height,0])	
+	var yApprecScale = d3.scaleLinear()
+			.domain([0,maxApprec])
+			.range([height,0])	
 
 
-	apprecGraph.selectAll('rect')
+	let apprecRects = apprecGraph.selectAll('rect')
 		.data(apprecData)
 		.enter()
 		.append('rect')
+		.attr('class','bar')
 		.attr('width',50)
 		.attr('x',function(d,i){ return i*100})
-		.attr('y',function(d){ return height - d.value})
 		.attr('y',function(d){ return yApprecScale(d.value) })
 		.attr('height',function(d){ return height - yApprecScale(d.value) })
 		.attr('fill','#4c6575');
 
+	apprecRects.on('mouseover',function(e){
+			console.log(e);
+		})
+
 	var yAxisApprecGen = d3.axisLeft(yApprecScale).ticks(5);
 		apprecGraph.append('g')
 			.call(yAxisApprecGen);
+
+	//tooltip for appreciations
+	var apprecTooltip = apprecGraph.append('g')
+			.style('opacity',0)
+			.attr('class','tooltip');
+
+
+	apprecTooltip.append('rect')
+			.attr('width',150)
+			.attr('height',50)
+			.attr('fill','rgba(0,0,0,0.8)');
+
+	var apprecTooltipText = apprecTooltip.append('text')
+							.text('bla')
+							.attr('class','gtext')
+							.attr('fill','white')
+							.attr('x',75)
+							.attr('y',25)
+							.style('alignment-baseline', 'middle')
+							.style('text-anchor', 'middle');
+
+	apprecBars = apprecGraph.selectAll('.bar');
+
+	//mouse events
+	apprecBars.on('mouseover',function(d){
+		apprecTooltip.style('opacity',1);
+		apprecTooltipText.text(d.name + ' : '+ d.value);
+	});
+
+	apprecBars.on('mouseout',function(d){
+		apprecTooltip.style('opacity',0);
+	});
+
+	apprecBars.on('mousemove',function(d){
+		//move the tooltip around
+		var mousePos = d3.mouse(this.parentNode);
+		var xPos = mousePos[0]-75;
+		var yPos = mousePos[1]-60;
+
+		apprecTooltip.attr('transform','translate('+xPos+','+yPos+')');
+
+
+	});
 
 
 	// ===========  GRAPH-3 FOLLOWERS  ============
@@ -253,24 +349,71 @@ $(function() {
 	follGraph.attr('transform','translate('+margin+','+margin+')')
 
 	var maxFoll = d3.max(follData, function(d) { return +d.value;} );
-		var yFollScale = d3.scaleLinear()
+	var yFollScale = d3.scaleLinear()
 				.domain([0,maxFoll])
 				.range([height,0])
 
-	follGraph.selectAll('rect')
+	let follRects = follGraph.selectAll('rect')
 		.data(follData)
 		.enter()
 		.append('rect')
+		.attr('class','bar')
 		.attr('width',50)
 		.attr('x',function(d,i){ return i*100})
-		.attr('y',function(d){ return height - d.value})
 		.attr('y',function(d){ return yFollScale(d.value) })
 		.attr('height',function(d){ return height - yFollScale(d.value) })
 		.attr('fill','#293e4f');
 
+	follRects.on('mouseover',function(e){
+			console.log(e);
+		})
+
 	var yAxisFollGen = d3.axisLeft(yFollScale).ticks(5);
 		follGraph.append('g')
 			.call(yAxisFollGen);
+
+	//tooltip for follows
+	var follTooltip = follGraph.append('g')
+			.style('opacity',0)
+			.attr('class','tooltip');
+
+
+	follTooltip.append('rect')
+			.attr('width',150)
+			.attr('height',50)
+			.attr('fill','rgba(0,0,0,0.8)');
+
+	var follTooltipText = follTooltip.append('text')
+							.text('bla')
+							.attr('class','gtext')
+							.attr('fill','white')
+							.attr('x',75)
+							.attr('y',25)
+							.style('alignment-baseline', 'middle')
+							.style('text-anchor', 'middle');
+
+	follBars = follGraph.selectAll('.bar');
+
+	//mouse events
+	follBars.on('mouseover',function(d){
+		follTooltip.style('opacity',1);
+		follTooltipText.text(d.name + ' : '+ d.value);
+	});
+
+	follBars.on('mouseout',function(d){
+		follTooltip.style('opacity',0);
+	});
+
+	follBars.on('mousemove',function(d){
+		//move the tooltip around
+		var mousePos = d3.mouse(this.parentNode);
+		var xPos = mousePos[0]-75;
+		var yPos = mousePos[1]-60;
+
+		follTooltip.attr('transform','translate('+xPos+','+yPos+')');
+
+
+	});
 
 
 // ===========  GRAPH-4 FOLLOWING  ============
@@ -284,20 +427,67 @@ $(function() {
 				.domain([0,maxFollowing])
 				.range([height,0])
 
-	followingGraph.selectAll('rect')
+	let followingRects = followingGraph.selectAll('rect')
 		.data(followingData)
 		.enter()
 		.append('rect')
+		.attr('class','bar')
 		.attr('width',50)
 		.attr('x',function(d,i){ return i*100})
-		.attr('y',function(d){ return height - d.value})
 		.attr('y',function(d){ return yFollowingScale(d.value) })
 		.attr('height',function(d){ return height - yFollowingScale(d.value) })
 		.attr('fill','#1a2330');
 
-		var yAxisFollowingGen = d3.axisLeft(yFollowingScale).ticks(5);
-		followingGraph.append('g')
-			.call(yAxisFollowingGen);
+	followingRects.on('mouseover',function(e){
+			console.log(e);
+	})
+
+	var yAxisFollowingGen = d3.axisLeft(yFollowingScale).ticks(5);
+	followingGraph.append('g')
+		.call(yAxisFollowingGen);
+
+	//tooltip for views
+	var followingTooltip = followingGraph.append('g')
+		.style('opacity',0)
+			.attr('class','tooltip');
+
+
+	followingTooltip.append('rect')
+			.attr('width',150)
+			.attr('height',50)
+			.attr('fill','rgba(0,0,0,0.8)');
+
+	var followingTooltipText = followingTooltip.append('text')
+							.text('bla')
+							.attr('class','gtext')
+							.attr('fill','white')
+							.attr('x',75)
+							.attr('y',25)
+							.style('alignment-baseline', 'middle')
+							.style('text-anchor', 'middle');
+
+	followingBars = followingGraph.selectAll('.bar');
+
+	//mouse events
+	followingBars.on('mouseover',function(d){
+		followingTooltip.style('opacity',1);
+		followingTooltipText.text(d.name + ' : '+ d.value);
+	});
+
+	followingBars.on('mouseout',function(d){
+		followingTooltip.style('opacity',0);
+	});
+
+	followingBars.on('mousemove',function(d){
+		//move the tooltip around
+		var mousePos = d3.mouse(this.parentNode);
+		var xPos = mousePos[0]-75;
+		var yPos = mousePos[1]-60;
+
+		followingTooltip.attr('transform','translate('+xPos+','+yPos+')');
+
+
+	});
 
 // ==========  END OF GRAPHS  ===========
 
@@ -309,7 +499,35 @@ $(function() {
 });
 		
 
+// var modal = document.getElementById('projects');
 
+// // Get the button that opens the modal
+// // var img = document.getElementById("history");
+// var img2 = document.getElementById("nzImg");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks the button, open the modal 
+// img.onclick = function() {
+//     modal.style.display = "block";
+// };
+
+// img2.onclick = function() {
+//     modal.style.display = "block";
+// };
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//     modal.style.display = "none";
+// };
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// };
 
 	
 
